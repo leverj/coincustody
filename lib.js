8962853d3718542e24c8dc2f7841b3faab4706df
Nirmal Gupta <nirmalk.gupta@gmail.com>
2017-09-20 16:08:41 -0700

    added standard token

---
+++ lib.js
@@ -0,0 +1,11 @@
+const DEBUG = require('debug');
+const path = require('path');
+
+function Debug(filename) {
+  return DEBUG("CUSTODY:" + path.basename(filename));
+}
+
+
+module.exports = {
+  Debug,
+};
