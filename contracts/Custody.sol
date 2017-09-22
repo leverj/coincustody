8084c61cbe5cce73eccec73f09a857189d686f10
Nirmal Gupta <nirmalk.gupta@gmail.com>
2017-09-21 22:35:11 -0700

    withdraw.

--- contracts/Custody.sol
+++ contracts/Custody.sol
@@ -14,6 +14,21 @@ import "tokens/HumanStandardToken.sol";
 
 contract Custody {
 
+    struct Order {
+    uint uuid;
+    uint price;
+    uint qty;
+    bool isBuy;
+    }
+
+    struct Execution {
+    uint executionid;
+    uint price;
+    uint qty;
+    }
+
+    event LOG(address _user, bytes32 _hash);
+
     event CustodyEvent(address _user, uint256 _value, string _unit, string _action);
 
     mapping (address => uint256) public ethers;
@@ -58,6 +73,56 @@ contract Custody {
         token.transferFrom(msg.sender, this, _value);
         //        token.delegatecall(bytes4(sha3("transfer(address,uin256)")), this, _value);
         return true;
+    }
+//nonce needs to be added to prevent replay attack
+    function withdraw(uint[] priceandqty,
+    uint[] ids,
+    bool[] isBuy,
+    address[] users,
+    uint256[] withdraws,
+    uint8[] v, bytes32[] r, bytes32[] s) onlyOwner {
+        // loop over all pending executions
+        Order memory order1 = Order(ids[0], priceandqty[0], priceandqty[1], isBuy[0]);
+        Order memory order2 = Order(ids[1], priceandqty[2], priceandqty[3], isBuy[1]);
+        Execution memory execution = Execution(ids[2], priceandqty[4], priceandqty[5]);
+        require(isVerified(order1, users[0], v[0], r[0], s[0]));
+        require(isVerified(order2, users[1], v[1], r[1], s[1]));
+        validateOrderAndExecution(order1, execution);
+        validateOrderAndExecution(order2, execution);
+        send(users[0],  withdraws[0], withdraws[1]);
+        send(users[1], withdraws[2], withdraws[3]);
+    }
+
+    function send(address _user, uint256 _eth, uint256 _tokens){
+        require(ethers[_user] >= _eth);
+        require(tokens[_user] >= _tokens);
+        ethers[_user] -= _eth;
+        tokens[_user] -= _tokens;
+        _user.transfer(_eth);
+        token.transfer(_user, _tokens);
+    }
+
+    function processExecution(address buyer, address seller, Execution execution) internal {
+        uint256 amount = execution.price * execution.qty;
+        tokens[buyer] += execution.qty;
+        tokens[seller] -= execution.qty;
+        ethers[buyer] -= amount;
+        ethers[seller] += amount;
+    }
+
+    function validateOrderAndExecution(Order order, Execution execution) internal {
+        if (order.isBuy) {
+            require(order.price >= execution.price);
+        }
+        else {
+            require(order.price <= execution.price);
+        }
+        require(order.qty >= execution.qty);
+    }
+
+    function isVerified(Order order, address user, uint8 v, bytes32 r, bytes32 s) internal returns (bool result){
+        bytes32 hash = keccak256(order.uuid, order.price, order.qty, order.isBuy);
+        return user == ecrecover(keccak256("\x19Ethereum Signed Message:\n32", hash), v, r, s);
     }
 
 
--- test/custody.js
+++ test/custody.js
@@ -1,6 +1,7 @@
 const StandardToken = artifacts.require("./HumanStandardToken.sol");
 const Custody = artifacts.require("./Custody.sol");
 
+let bluebird = require('bluebird');
 const expect = require("expect.js");
 const fs = require('fs');
 const BN = require('bn.js');
@@ -10,7 +11,8 @@ const EthQuery = require('ethjs-query');
 const Web3 = require('web3');
 const lib = require('../lib');
 const debug = lib.Debug(__filename);
-
+const abi = require('ethereumjs-abi');
+const ethUtil = require('ethereumjs-util');
 
 const ethRPC = new EthRPC(new HttpProvider('http://localhost:8545'));
 const ethQuery = new EthQuery(new HttpProvider('http://localhost:8545'));
@@ -37,10 +39,63 @@ contract('deposit', function (accounts) {
   });
 });
 
+contract('Order posted', function (accounts) {
+  let token, custody;
+  let user1 = accounts[1];
+  let user2 = accounts[2];
+
+  before(async function () {
+    [token, custody] = await setup(accounts);
+    await lib.sendToken(100, user1, custody, token);
+    await web3.eth.sendTransaction({from: user1, to: custody.address, value: 10000000});
+    await lib.sendToken(200, user2, custody, token);
+    await web3.eth.sendTransaction({from: user2, to: custody.address, value: 20000000});
+  });
+  /**
+   * orders: price, quantity
+   * signature
+   * execution, user1 order, user2 order check signature
+   */
+  it('user can withdraw through exchange', async function () {
+    let order1 = {uuid: 1, price: 10, qty: 100, isBuy: true, user: user1};
+    let order2 = {uuid: 2, price: 10, qty: 100, isBuy: false, user: user2};
+    let execution = {uuid: 3, price: 10, qty: 100};
+    let types = ['uint', 'uint', 'uint', 'bool'];
+    let [v1, r1, s1] = await getSignature(types, [order1.uuid, order1.price, order1.qty, order1.isBuy], order1.user);
+    let [v2, r2, s2] = await getSignature(types, [order2.uuid, order2.price, order2.qty, order2.isBuy], order2.user);
+    await custody.withdraw([order1.price, order1.qty, order2.price, order2.qty, execution.price, execution.qty],
+      [order1.uuid, order2.uuid, execution.uuid],
+      [order1.isBuy, order2.isBuy],
+      [order1.user, order2.user], [10, 10, 11, 11], [v1, v2], [r1, r2], [s1, s2]);
+
+    expect(await lib.balance(user1, token)).to.be.eql(9910);
+    expect(await lib.balance(user2, token)).to.be.eql(9811);
+    expect(await lib.balance(custody.address, token)).to.be.eql(279);
+// fixme: later
+    // expect((await custody.ethers(user1)).toNumber()).to.eql(10000000-10);
+    // expect((await custody.tokens(user1)).toNumber()).to.eql(100 -10);
+    // expect((await custody.ethers(user1)).toNumber()).to.eql(20000000 -11);
+    // expect((await custody.tokens(user1)).toNumber()).to.eql(200 -11);
+    //
+    // expect(await web3.eth.getBalance(custody.address)).to.eql(10000000);
+
+  });
+});
+
+
+async function getSignature(types, values, user) {
+  let hash = abi.soliditySHA3(types, values);
+  console.log('user', user, 'types', types, 'values', values, 'hash', ethUtil.bufferToHex(hash));
+  let sig = await web3.eth.sign(ethUtil.bufferToHex(hash), user);
+  let {v, r, s} = ethUtil.fromRpcSig(sig);
+  return [v, ethUtil.bufferToHex(r), ethUtil.bufferToHex(s)];
+}
+
 async function setup(accounts) {
   let custody = await Custody.deployed();
   let token = await StandardToken.new(1000000, "some awesome token", 0, "SAT");
   await token.transfer(accounts[1], 10000);
+  await token.transfer(accounts[2], 10000);
   await custody.setToken(token.address);
   return [token, custody];
 }
